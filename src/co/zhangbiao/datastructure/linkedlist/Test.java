a9c227a03dead71eca69a276ec31ad4803a3216a
zhangbiao <zhangbiao97@126.com>
2020-04-25 17:50:23 +0800

    测试

--- src/co/zhangbiao/datastructure/linkedlist/Test.java
+++ src/co/zhangbiao/datastructure/linkedlist/Test.java
@@ -9,17 +9,11 @@ public class Test {
     public static void main(String[] args) {
 
         LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 3; i++) {
             linkedList.addFirst(i);
         }
         System.out.println(linkedList);
-        linkedList.add(2, 666);
-        System.out.println(linkedList);
-        linkedList.remove(2);
-        System.out.println(linkedList);
-        linkedList.removeFirst();
-        System.out.println(linkedList);
-        linkedList.removeLast();
+        linkedList.add(1, 3);
         System.out.println(linkedList);
 
     }
