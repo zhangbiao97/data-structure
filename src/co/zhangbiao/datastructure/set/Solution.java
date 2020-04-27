7dd56c6402a36b545c64a81e6d68642ffbfb9338
zhangbiao <zhangbiao97@126.com>
2020-04-27 13:35:54 +0800

    解答LeetCode一题

---
+++ src/co/zhangbiao/datastructure/set/Solution.java
@@ -0,0 +1,28 @@
+package co.zhangbiao.datastructure.set;
+
+import java.util.TreeSet;
+
+/**
+ * 描述：解答LeetCode中804.唯一摩尔斯密码词一题。
+ * <p>
+ * Create By ZhangBiao
+ * 2020/4/27
+ */
+public class Solution {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-." +
+                ".", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-" +
+                ".--", "--.."};
+        TreeSet<String> set = new TreeSet<>();
+        for (String word : words) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                result.append(codes[word.charAt(i) - 'a']);
+            }
+            set.add(result.toString());
+        }
+        return set.size();
+    }
+
+}
